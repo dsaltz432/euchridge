a2e397281f2a1b7c345f982163cd19cb15b8db99
Daniel Saltz <dsaltz@u.rochester.edu>
2019-08-21 11:20:54 -0500

    complies with linting

--- src/Content.js
+++ src/Content.js
@@ -1,31 +1,35 @@
 import React from 'react';
-import { Link } from 'react-scroll'
-import getSections from "./Api";
+import { Link } from 'react-scroll';
+import getSections from './Api';
 import './Content.css';
 
 const Titles = props => {
-  const elements = props.sections.map((section, index) => {
-    let uniqueId = section + '_' + index;
+  const { sections } = props;
+  const elements = sections.map((section, index) => {
+    const uniqueId = `${section}_${index}`;
     return (
-      <li key={index}>
-        <Link activeClass='active' className={uniqueId} to={uniqueId} spy={true} smooth={true} duration={500}>{section.title}</Link>
+      <li>
+        <Link activeClass="active" className={uniqueId} to={uniqueId} spy smooth duration={500}>
+          {section.title}
+        </Link>
       </li>
-      );
+    );
   });
-  return <ul>{elements}</ul>
+  return <ul>{elements}</ul>;
 };
 
 const Descriptions = props => {
-  const elements = props.sections.map((section, index) => {
-    let uniqueId = section + '_' + index;
+  const { sections } = props;
+  const elements = sections.map((section, index) => {
+    const uniqueId = `${section}_${index}`;
     return (
       <div id={uniqueId}>
-      <h2>{section.title}</h2>
-      <p>{section.desc}</p>
+        <h2>{section.title}</h2>
+        <p>{section.desc}</p>
       </div>
-      );
+    );
   });
-  return <div>{elements}</div>
+  return <div>{elements}</div>;
 };
 
 export default class Content extends React.Component {
@@ -34,13 +38,13 @@ export default class Content extends React.Component {
     return (
       <div>
         <div className="sidenav">
-          <Titles sections={sections}/>
+          <Titles sections={sections} />
         </div>
         <div className="main">
           <h1>Introduction to Euchridge</h1>
-          <Descriptions sections={sections}/>
+          <Descriptions sections={sections} />
         </div>
       </div>
     );
   }
-};
+}
