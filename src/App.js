bccd0becce86b7a81b3637fbd7d5a2dd7637f2ec
Daniel Saltz <dsaltz@u.rochester.edu>
2019-08-23 16:48:08 -0500

    added basic Standings component with react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { MenuList, MenuItem } from '@material-ui/core';
 import Rules from './Rules';
+import Standings from './Standings';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Rules />
-      </div>
+      <Router>
+        <div>
+          <MenuList style={{ backgroundColor: 'grey', display: 'inline-block', width: '100%' }}>
+            <MenuItem style={{ display: 'inline-block', float: 'right' }}>
+              <Link to="/rules" style={{ color: 'white', textDecoration: 'none' }}>
+                Rules
+              </Link>
+            </MenuItem>
+            <MenuItem style={{ display: 'inline-block', float: 'right' }}>
+              <Link to="/standings" style={{ color: 'white', textDecoration: 'none' }}>
+                Standings
+              </Link>
+            </MenuItem>
+          </MenuList>
+          <Switch>
+            <Route exact path="/rules" component={Rules} />
+            <Route path="/standings" component={Standings} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
